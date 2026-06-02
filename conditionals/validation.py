3dc2f0578b5257f93de869dea04888b5f5b62a34
onyejekeemmanuella <emmanuellachi009@gmail.com>
2026-06-02 15:41:46 +0100

    validation in python using conditionals

---
+++ conditionals/validation.py
@@ -0,0 +1,25 @@
+# 
+
+username=input("enter username: ")
+password=input("enter password: ")
+constantUsername = "ella"
+constantPassword= "ella54"
+
+if(username==constantUsername and password==constantPassword):
+    print("logged in")
+else:
+    print("invalid credentials")
+
+# if(username==constantUsername or password== constantPassword):
+#     print("logged in")
+# else:
+#     print("invalid credentials")
+
+# print(f"hello {username} your password is {password}")
+# if(username=="ella"):
+#     print("logged in")
+# else:
+#     print("invalid credential")
+    
+
+    
