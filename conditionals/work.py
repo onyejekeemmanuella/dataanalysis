26e1c23f2f30626f45d56bc55c3822e8d9c86dbf
onyejekeemmanuella <emmanuellachi009@gmail.com>
2026-06-02 15:52:36 +0100

    work

---
+++ conditionals/work.py
@@ -0,0 +1,10 @@
+username=input("enter username: ")
+password=input("enter password: ")
+constantUsername= "interkel"
+constantPassword= "int3450"
+
+if(username==constantUsername and password==constantPassword):
+    print("verified")
+else:
+    print("unverified")
+    
