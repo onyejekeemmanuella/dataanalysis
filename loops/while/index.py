0cb261b0c2932acb1413fa442e763f573ca44e80
onyejekeemmanuella <emmanuellachi009@gmail.com>
2026-06-02 17:26:45 +0100

    whileloop

---
+++ loops/while/index.py
@@ -0,0 +1,13 @@
+# i = 0
+# while i != 30:
+#     i+= 1
+#     print(i)
+# name = input("enter name: ")
+# while name!= "Ella":
+#     name = input("enter name: ")
+# print ("welcome Ella")
+i = 1
+while i != 0:
+    i += 1
+    print(i)
+ 
